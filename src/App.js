d7ceeb5037dea5d7b9ef0c4313cb1d697898b224
utkutasglk <utku.tasguluk@gmail.com>
2025-03-27 12:28:25 +0300

    Landing page was comleted.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from "./Components/Home";
 import About from "./Components/About";
 import Work from "./Components/Work";
 import Testimonials from "./Components/Testimonials";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <About />
       <Work />
       <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="contact-page-wrapper">
+      <h1 className="primary-heading">Have Question in Mind ?</h1>
+      <h1 className="primary-heading">Let us Help you</h1>
+      <div className="contact-form-container">
+        <input type="text" placeholder="utku.tasguluk@gmail.com" />
+        <button className="secondary-button">Submit</button>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
